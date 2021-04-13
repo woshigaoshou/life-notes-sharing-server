f33c7bcd55c5eabb71d3553cc60620b9c324bde9
woshigaoshou <1258006989@qq.com>
2021-04-13 23:28:43 +0800

    把Api样例写一遍

--- router.js
+++ router.js
@@ -3,15 +3,43 @@ var router = express.Router()
 
 const User = require('./modules/userInfo');
 
-router.get('/', function (req, res) {
-  // const user = User();
-  User.create({
+router.get('/save', function (req, res) {
+  const user = new User({
     name: '张三',
     account: 'account1',
     password: '123',
-  }).then(res => {
+  });
+  user.save();
+  // User.create({
+  //   name: '张三',
+  //   account: 'account1',
+  //   password: '123',
+  // }).then(res => {
+  //   console.log(res);
+  // })
+  res.send('save');
+})
+router.get('/update', function (req, res) {
+  const query = { account: 'account1' };
+  const doc = { password: 123456 };
+  User.update(query, doc, function (req, res) {
     console.log(res);
   })
-  res.send('hello!');
+  res.send('update');
+})
+router.get('/delete', function (req, res) {
+  const query = { account: 'account1' };
+  const doc = { password: 123456 };
+  User.update(query, doc, function (req, res) {
+    console.log(res);
+  })
+  res.send('delete');
+})
+router.get('/find', function (req, res) {
+  const query = { name: '张三' };
+  User.find(query, function (req, res) {
+    console.log(res);
+  })
+  res.send('find');
 })
 module.exports = router
